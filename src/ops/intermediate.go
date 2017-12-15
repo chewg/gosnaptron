e1aa2dbce6587a49d6aaf5138e75bc3c8cdea249
chewg <cwang@cs.jhu.edu>
2017-12-15 13:08:42 -0500

    Rename Intersection vars, add comments

--- src/ops/intermediate.go
+++ src/ops/intermediate.go
@@ -62,33 +62,34 @@ func Intersect(frames_1 *[]Frame, more_frames ...*[]Frame) *[]Frame {
 
 	// Intersection procedure
 	for _, frames := range more_frames {
-		common_m := map[int]Frame{}
+		intersected_m := map[int]Frame{}
 
 		for _, frame := range *frames {
 			frame_id := get_frame_id(&frame)
 
 			// Check if have intersection
-			if m_frame, in_m := m[frame_id]; in_m {
-				if common_frame, in_common := common_m[frame_id]; in_common {
+			if stored_frame, intersection_exists := m[frame_id]; intersection_exists {
+				if intersected_frame, stored_in_intersected_m := intersected_m[frame_id]; stored_in_intersected_m {
+					// In intersected_m, so update intersected_frame
 					switch group_frames_by.(type) {
 					case By_Sample_ID:
-						common_m[frame_id] = update_by_sample_id(common_frame, frame)
+						intersected_m[frame_id] = update_by_sample_id(intersected_frame, frame)
 					case By_Junction_ID:
-						common_m[frame_id] = update_by_junction_id(common_frame, frame)
+						intersected_m[frame_id] = update_by_junction_id(intersected_frame, frame)
 					}
 				} else {
-					// Not in common_m, so update count and add to common_m
+					// Not in intersected_m, so add stored_frame to intersected_m
 					switch group_frames_by.(type) {
 					case By_Sample_ID:
-						common_m[frame_id] = update_by_sample_id(frame, m_frame)
+						intersected_m[frame_id] = update_by_sample_id(frame, stored_frame)
 					case By_Junction_ID:
-						common_m[frame_id] = update_by_junction_id(frame, m_frame)
+						intersected_m[frame_id] = update_by_junction_id(frame, stored_frame)
 					}
 				}
 			}
 		}
 
-		m = common_m
+		m = intersected_m
 	}
 
 	return convert_map_to_slice(&m)
@@ -101,6 +102,15 @@ func Order(frames *[]Frame, order ...order_func) *[]Frame {
 }
 
 
+/*****
+Summarize
+
+Takes a slice of frames and performs summary statistic on each of the frames in the slice.
+For example, one can pass in ops.Sum_Count as a stat_func which does a summation of each frame's count
+
+Parameters: pointer to 1 slice of frames, 1 or more stat functions found in stat.go
+Output: address of 1 slice of frames, which has had stat_func performed on it
+*****/
 func Summarize(frames *[]Frame, stats ...stat_func) *[]Frame {
 	for _, stat := range stats {
 		frames = stat(frames)
@@ -110,14 +120,24 @@ func Summarize(frames *[]Frame, stats ...stat_func) *[]Frame {
 }
 
 
+/*****
+Union
+
+Takes slices of frames, and based on the frame id the frames are grouped by and is globally set,
+the different slices are unioned together.
+
+Parameters: pointer to 1 slice of frames, pointer(s) to 1 or more slice(s) of frames
+Output: address of 1 slice of frames, which is the union of all frames grouped by the frame id
+*****/
 func Union(frames_1 *[]Frame, more_frames ...*[]Frame) *[]Frame {
 	m := *convert_slice_to_map(frames_1)
 
-	// Union procedure
+	// iterate through more_frames and union each one with frames_1
 	for _, frames := range more_frames {
 		for _, frame := range *frames {
 			frame_id := get_frame_id(&frame)
 
+			// union by frame_id
 			if saved_frame, exist := m[frame_id]; exist {
 				switch group_frames_by.(type) {
 				case By_Sample_ID:
@@ -133,4 +153,3 @@ func Union(frames_1 *[]Frame, more_frames ...*[]Frame) *[]Frame {
 
 	return convert_map_to_slice(&m)
 }
-
